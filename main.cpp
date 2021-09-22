39fca7972a4fff48d2ac12f2898591b77623b3af
progenor <nero6126@gmail.com>
2021-09-22 23:00:39 +0300

    asdasdads

--- main.cpp
+++ main.cpp
@@ -2,9 +2,27 @@
 
 using namespace std;
 
+
 int main() {
 
-cout<<"szia";
+    char s[]="parosa paratla szam";
+    int h=0;
+
+    for(int i =0; s[i]!='\0';i++)
+    {
+
+        if(s[i]!=' ')
+            h++;
+        else if(h%2!=0)
+        {
+            for(int j=i-h;j<i;j++)
+                s[j]='x';
+            h=0;
+        }
+    }
+
+    cout<<s;
+
 
     return 0;
 }
